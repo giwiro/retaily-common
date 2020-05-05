dbe577996f2337ca11abf55be872b45cf1af7cf4
giwiro <giwirodavalos@gmail.com>
2020-05-05 14:12:56 -0500

    updated componentscan

--- src/main/kotlin/com/retaily/common/web/Authorized.kt
+++ src/main/kotlin/com/retaily/common/web/Authorized.kt
@@ -3,6 +3,7 @@ package com.retaily.common.web
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
 
@@ -12,6 +13,7 @@ annotation class Authorized()
 
 @Aspect
 @Configuration
+@ComponentScan
 open class AuthAspect(@Autowired val sessionService: SessionService) {
     @Before("@annotation(com.retaily.common.web.Authorized)")
     fun before() {
